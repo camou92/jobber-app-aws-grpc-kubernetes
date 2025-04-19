7adabc60828b003bee4378847c813d198eac2383
camou92 <cmohamed992@gmail.com>
2025-04-19 23:26:54 +0100

    fixed test

--- apps/jobber-jobs/jest.config.ts
+++ apps/jobber-jobs/jest.config.ts
@@ -1,6 +1,6 @@
 export default {
   displayName: 'jobber-jobs',
-  preset: '../jest.preset.js',
+  preset: '../../jest.preset.js',
   testEnvironment: 'node',
   transform: {
     '^.+\\.[tj]s$': ['ts-jest', { tsconfig: '<rootDir>/tsconfig.spec.json' }],
